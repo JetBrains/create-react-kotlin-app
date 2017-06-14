e04ebff1b26f10393fb79306ec9f960c2d7d7e53
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-06-14 17:47:00 +0300

    CRKA-13 if "packageName" is not passed, download every package from package.json

--- packages/ts2kt-automator/index.js
+++ packages/ts2kt-automator/index.js
@@ -3,17 +3,12 @@
 const lib = require('./lib');
 const minimist = require('minimist');
 const mkdirp = require('mkdirp');
+const fs = require('fs');
 
 const argv = minimist(process.argv.slice(2));
 
 const packageName = argv.packageName;
-const dest = `${argv.dest}/${packageName}`;
-
-if (!packageName) {
-  throw new Error(
-    'Package name should be specified like: --packageName=jquery'
-  );
-}
+const dest = argv.dest;
 
 if (!argv.dest) {
   throw new Error(
@@ -21,12 +16,62 @@ if (!argv.dest) {
   );
 }
 
-mkdirp(dest, function() {
-  lib
-    .installTypes(packageName)
-    .then(() => lib.convertTypesToKotlin(packageName, dest))
+function installOnePackage(packageName, destFolder) {
+  const [name] = packageName.split('@');
+  const dest = `${destFolder}/${name}`;
+
+  return new Promise((resolve, reject) => {
+    mkdirp(dest, function() {
+      lib
+        .installTypes(name)
+        .then(() => lib.convertTypesToKotlin(name, dest))
+        .catch(reject)
+        .then(resolve);
+    });
+  });
+}
+
+function installAllPackages() {
+  console.log(
+    'We\'ll download every package from "dependencies" in package.json which is not downloaded yet.'
+  );
+  const dependencies = lib.getPackageDependencies();
+
+  Object.keys(dependencies)
+    .reduce(
+      (promise, packageName) => {
+        const [name] = packageName.split('@');
+        const packageDest = `${dest}/${name}`;
+
+        if (fs.existsSync(packageDest)) {
+          console.log(
+            `Path "${packageDest}" already exists and will not be overridden.`
+          );
+          return promise;
+        }
+
+        return promise.then(() => installOnePackage(name, dest)).catch(err => {
+          try {
+            fs.unlinkSync(packageDest);
+          } catch (err) {
+            console.error('Cannot clear folder after error', packageDest, err);
+          }
+          console.error(err);
+        });
+      },
+      Promise.resolve()
+    )
     .catch(err => {
       console.error(err);
       process.exit(1);
     });
-});
+}
+
+if (!packageName) {
+  installAllPackages();
+} else {
+  installOnePackage(packageName, dest).catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+}
