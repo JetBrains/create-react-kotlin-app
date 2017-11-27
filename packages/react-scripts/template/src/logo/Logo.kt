9c74118f68ed6c4bb4a81b4284b6986c4a0f91ab
hypnos <Filipp.Riabchun@jetbrains.com>
2017-11-28 02:01:41 +0300

    [Publish] Update dependencies (including kotlin@1.2.0-rc)

--- packages/react-scripts/template/src/logo/Logo.kt
+++ packages/react-scripts/template/src/logo/Logo.kt
@@ -12,9 +12,7 @@ external val kotlinLogo: dynamic
 
 fun RBuilder.logo(height: Int = 100) {
     div("Logo") {
-        attrs.style = js {
-            this.height = height
-        }
+        attrs.jsStyle.height = height
         img(alt = "React logo.logo", src = reactLogo, classes = "Logo-react") {}
         img(alt = "Kotlin logo.logo", src = kotlinLogo, classes = "Logo-kotlin") {}
     }
