99b0422bd28e43b7b15dc82ea0362be8abd8dc88
hypnos <Filipp.Riabchun@jetbrains.com>
2017-11-29 02:45:56 +0300

    Update dependencies. Improved fragment support. Keys are no more necessary when using fragments [Publish patch versions]

--- packages/react-scripts/template/src/app/App.kt
+++ packages/react-scripts/template/src/app/App.kt
@@ -8,20 +8,17 @@ import ticker.*
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
         div("App-header") {
-            key = "header"
             logo()
             h2 {
                 +"Welcome to React with Kotlin"
             }
         }
         p("App-intro") {
-            key = "intro"
             +"To get started, edit "
             code { +"app/App.kt" }
             +" and save to reload."
         }
         p("App-ticker") {
-            key = "ticker"
             ticker()
         }
     }
