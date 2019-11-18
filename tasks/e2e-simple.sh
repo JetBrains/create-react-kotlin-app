458de5c47848d7506ab5f4f440be51738020f91a
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2019-11-18 23:31:42 +0300

    [Publish patch versions] publish

--- tasks/e2e-simple.sh
+++ tasks/e2e-simple.sh
@@ -256,6 +256,7 @@ rm -rf "$root_path"/packages/kotlin-webpack-plugin/node_modules/.bin
 rm -rf "$root_path"/packages/react-scripts/node_modules/.bin
 rm -rf "$root_path"/packages/kotlinc-js-api/node_modules/.bin
 rm -rf "$root_path"/packages/ts2kt-automator/node_modules/.bin
+
 npm link "$root_path"/packages/kotlin-webpack-plugin
 npm link "$root_path"/packages/kotlinc-js-api
 npm link "$root_path"/packages/react-scripts
